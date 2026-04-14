83ad82b6a331b6bfcd502812ac93d3327b3818dd
MrGame021 <130846611+MrGame021@users.noreply.github.com>
2026-04-14 17:18:26 -0300

    Add files via upload

---
+++ Aula 7/backend_add.php
@@ -0,0 +1,19 @@
+<?php
+
+
+//conexao com o banco
+$conect = new mysqli('localhost', 'root', '', 'aula6');
+
+// coleta de info
+$nome = @$_POST['nome'];
+$idade = @$_POST['idade'];
+$cidade = @$_POST['cidade'];
+
+// Construindo comando em sql
+$sql = "INSERT INTO pessoas (nome, idade, cidade) VALUES ('$nome', '$idade', '$cidade')";
+//executa 
+$conect->query($sql);
+
+// voltar ao forms instantaneamente
+header('Location: frontend_forms.php')
+?>
---
+++ Aula 7/frontend_forms.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1>Adicionar pessoa:</h1>
+    
+    <form action="backend_add.php" method="post">
+        <input type="text" name='nome' placeholder='Nome...'>
+        <input type="number" name='idade' placeholder='Idade...'>
+        <input type="text" name='cidade' placeholder='Cidade...'>
+        <button type='submit'>Enviar</button>
+    </form>
+
+</body>
+</html>
