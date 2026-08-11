e5287ff484fd348c449bf5f11eeb234912a549dc
MrGame021 <130846611+MrGame021@users.noreply.github.com>
2026-08-11 17:51:25 -0300

    Add files via upload

---
+++ Aula 21 - pt13/back_cadastro.php
@@ -0,0 +1,85 @@
+<?php
+session_start();
+
+include_once('conect.php');
+
+$nome = @$_POST['nome']; //Nome
+$senha = @$_POST['senha']; //senha
+$senhaC = @$_POST['senha_Conf'];//confirmação de senha( == senha)
+$date = @$_POST['date'];//nascimento
+$email = @$_POST['email'];//email(unico)
+$gen = @$_POST['genero'];//genero
+
+// manter informações
+$_SESSION['old'] = $_POST;
+
+$dataA = new DateTime();
+$dataA->modify('-1 day');//exclui as ultimas 24h, ou seja, dia atual nn pode ser seu nascimento
+$dataU = new DateTime($date);//nascimento do usuario
+
+$erros = [];
+
+$sql = "SELECT * FROM `usuarios` WHERE email = '$email'";
+$result = $conect->query($sql);
+
+//----------------------------------------------------------------
+
+if(empty($nome)){  //Nome vazio
+    $erros[] = "<script>alert('Você não colocou seu nome')</script>";
+}
+else if(strlen($nome) < 3){  //Nome menor que 3 carac.
+    $erros[] = "<script>alert('Seu nome tem que ter, no mínimo, 3 caracteres')</script>";
+}
+else if(strlen($nome) > 100){  //Nome maior que 100 carac.
+    $erros[] = "<script>alert('Seu nome não pode ter mais de 100 caracteres')</script>";
+}
+else{ 
+    if(strlen($senha) <= 7){  //Senha menor que 8 carac.
+        $erros[] = "<script>alert('Senha tem que ter um mínimo de 8 caracteres')</script>";
+    }
+    else if(strlen($senha) > 100){  //Senha maior que 100 carac.
+        $erros[] = "<script>alert('Escolha uma senha menor que 100 caracteres')</script>";
+    }
+    else if($senha != $senhaC){  //Senha e senha confirmada igualadas
+        $erros[] = "<script>alert('Senha ou confirmação de senha incorretos')</script>";
+    }
+    else{
+        if(empty($date)){  //Data nasc. vazio
+            $erros[] = "<script>alert('Você deve selecionar sua data de nascimento')</script>";
+        }
+        else if($dataA <= $dataU){  //data de hoje igual a data de nasc. || data futuro a atual
+            $erros[] = "<script>alert('Você ainda não nasceu. Volte mais tarde')</script>";
+        }
+        else{
+            if(empty($email)){  //Email vazio
+                $erros[] = "<script>alert('Você precisa preencher o email')</script>";
+            }
+            else if(strlen($email) > 150){  //Email grande demais
+                $erros[] = "<script>alert('Escolha um email com menos de 150 caracteres')</script>";
+            }
+            else if($result->num_rows > 0){
+                $erros[] = "<script>alert('Este email já foi cadastrado anteriormente. Por favor, troque para poder prosseguir.')</script>";
+            }
+                if(empty($gen)){  //Genero vazio
+                    $erros[] = "<script>alert('Caso não queira preencher, selecione a opção neutra')</script>";
+                }
+            }
+        }
+}
+
+// CRIPTOGRAFIA
+$senhaCrip = password_hash($senha, PASSWORD_DEFAULT);
+
+if(count($erros)>0){
+    $_SESSION['erros'] = $erros;
+    header('Location: sist_cadastro.php');
+    var_dump($erros);
+}
+else{
+    $sql = "INSERT INTO usuarios (NOME, SENHA, DATA_NASC, EMAIL, GENERO) VALUES ('$nome', '$senhaCrip', '$date', '$email', '$gen')";
+    $conect->query($sql);
+    header('Location: sist_login.php');
+}
+
+
+?>
---
+++ Aula 21 - pt13/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <a id="out" href="back_logout.php">
+        <div class="logout">
+        <?php
+            session_start();
+
+            $conect = new mysqli('localhost', 'root', '', 'usuarios');
+            if(!isset($_SESSION['id'])){
+                header('Location: sist_login.php');        
+            }
+            else{
+                echo('<h1>Desconectar</h1>');
+            }
+
+            ?>
+        </div>
+    </a>
+
+    <div class="corpoP">
+
+        <h1>Criar Postagem</h1>
+        <form action="back_post.php" method="post">
+            <small>Título</small><input name="titulo" type="text">
+            <small>Texto</small><textarea name='text'></textarea>    
+
+            <button type='submit'>Postar</button>
+        </form>
+
+        <main id='teste'>
+            <?php            
+            include_once('conect.php');
+            $sql = "SELECT * FROM posts ORDER BY id DESC LIMIT 5";
+            
+            $result = $conect->query($sql);
+
+            while($post = $result->fetch_assoc()){
+                $nome_id = $post['id_user'];
+                $sql = "SELECT * FROM usuarios WHERE ID = '$nome_id' ";
+                $result2 = $conect->query($sql);
+                $autor = $result2->fetch_assoc();
+
+                echo('<div class="post">');
+                echo('<h2>'.$post['titulo'].'</h2>');
+                echo('<p>'.$post['texto'].'</p>');
+                echo('<small>Autor: '.$autor['NOME'].'</small>');
+                echo('<small>Data: '.$post['data'].'</small>');
+                if($post['id_user'] == $_SESSION['id']){
+                    echo('<a href="post_delet.php?id='.$post['id'].'"> X </a>');
+                }
+                echo('</div>');
+            }
+            ?>
+        </main>
+    </div>
+    <div class="logout">
+        <h1>Perfil:</h1>
+    </div>
+
+</body>
+</html>
